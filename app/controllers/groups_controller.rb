8ec2909dd6cdcfdb74db9e66a9f96f5f235dc5a3
wangxing <18912540921@163.com>
2017-03-01 13:23:33 +0800

    add post and hook group/user relation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -41,14 +41,18 @@ class GroupsController < ApplicationController
     redirect_to groups_path, alert: "Group deleted"
   end
   private
-  def find_group_and_check_permission
-      @group = Group.find(params[:id])
+  
 
-      if current_user != @group.user
-        redirect_to root_path, alert: "You have no permission."
-      end
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
     end
+  end
+
   def group_params
     params.require(:group).permit(:title, :description)
   end
+
 end
